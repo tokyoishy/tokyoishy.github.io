82ef860281b71f38d0b20368aa88d4101530cdb7
tokyo <71803648+tokyoishy@users.noreply.github.com>
2023-08-05 22:21:31 -0400

    added memojis + picked randomly, banner on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import Typed from 'react-typed';
-import memoji from '../assets/memoji_2.mp4';
+import memoji2 from '../assets/memoji_2.mp4';
+import memoji from '../assets/memoji_1.mp4';
+import memoji3 from '../assets/memoji_3.mp4'
 import styles from '../index.css';
 import down_arrow from '../assets/down-arrow.png';
 import {Helmet} from 'react-helmet'
 
 const Hero = () => {
 
+    var memojiArray = [
+        memoji,
+        memoji2,
+        memoji3
+        ]
+      
+      function memojis(){
+        var randomNum = Math.floor(Math.random() * memojiArray.length)
+        return memojiArray[randomNum]
+      }
+
     return (
         <div id="home" className='text-black'>
             <meta name="viewport" content="width=device-width"></meta>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
                 <div className='memoji mx-auto'>
-                    <video autoPlay loop muted src={memoji} width="300" height="300" autoplay="true" playsinline="true" />
+                    <video id='test-mem' autoPlay loop muted src={memojis()} width="300" height="300" autoplay="true" playsinline="true" />
                 </div>
                 <div className='box'>
-                    <div className='flex text-center mx-auto justify-center py-5'>
+                    {/* <div className='flex text-center mx-auto justify-center py-5'>
                         <img className=' downarrow w-[50px]' src={down_arrow} alt="/" />
-                    </div>
-                    <h1 className='herotext md:text-7xl sm:text-6xl text-4xl font-bold md:py-3 sm:py-5'>hey there!</h1>
+                    </div> */}
+                    <h1 className='herotext md:text-6xl sm:text-5xl text-4xl font-bold'>hey there!</h1>
                 </div>
                 <div className='typed-tagline flex justify-center items-center'>
                     <p className='text-[black] md:text-5xl sm:text-4xl text-xl font-bold py-4'>i work with</p>
